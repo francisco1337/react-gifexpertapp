25ef02c7deb534c07fda33fa5376611070f7d1d0
Francisco del Villar <francisco.sdv.mtz@hotmail.com>
2021-07-13 23:28:50 -0500

    first commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const AddCategory = ({setCategories}) => {
+    const [InputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        console.log(e.target.value);
+        setInputValue( e.target.value);
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if( InputValue.trim().length > 2){
+
+            console.log('submit');
+            setCategories( cats => [ InputValue, ...cats])
+    
+        }
+
+    }
+
+    return (
+        
+        <form onSubmit={ handleSubmit }>
+          <input 
+            type="text" 
+            value={InputValue}
+            
+            onChange={ handleInputChange   }
+          />  
+        </form>
+        
+    )
+}
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+
+
---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['tetas']);
+
+    // const handleAdd = () => {
+    //     setCategories([...categories, 'porno']);
+
+    //     console.log(categories);
+    // }
+
+    // console.log(categories);
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr/>
+            
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid
+                            key= {category}
+                            category={ category } 
+                        />
+                    ))
+                }
+            </ol>
+
+            { categories }
+
+        </>
+    )
+}
+
+
+export default GifExpertApp;
